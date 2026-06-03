6b2bf965e1b1aabd036c3198b4351d9c2b4f6903
12500305 <12500305@student.pxl.be>
2026-06-03 13:57:43 +0200

    More answers

--- LakeLINQ/Program.cs
+++ LakeLINQ/Program.cs
@@ -78,13 +78,86 @@ namespace LakeLINQ
             Console.WriteLine($"De grootste vis is {biggestSize}cm groot");
 
             //Vind de gemiddelde grootte van alle vissen
+            double averageSize = fish.Average(f => f.SizeCm);
+            Console.WriteLine($"De gemiddelde vis is {averageSize}cm groot");
 
             //Bereken de lengte van alle vissen samen
+            int totalSize = fish.Sum(f => f.SizeCm);
+            Console.WriteLine($"Alle vissen samen zijn {totalSize} cm groot");
 
+            //Ga na of er een vis is die kleiner is dan 5cm
+            bool isSuperSmall = fish.Any(f => f.SizeCm < 5);
+            if (isSuperSmall)
+            {
+                Console.WriteLine("Er is één hele kleine vis");
+            } else
+            {
+                Console.WriteLine("Er is geen hele kleine vis");
+            }
+
+            //Groepeer vissen op habitat
+            var GroupedByCategory = fish.GroupBy(f => f.Habitat);
+            foreach (var group in GroupedByCategory)
+            {
+                Console.WriteLine($"Categorie: {group.Key}");
+                foreach(var f in group)
+                {
+                    Console.WriteLine($"{f.Name}");
+                }
+            }
+
+            //Ga na of alle vissen predators zijn
+            bool isAllPredators = fish.All(f => f.IsPredator == true);
+            if (isAllPredators)
+            {
+                Console.WriteLine("Alle vissen zijn roofdieren");
+            }
+            else
+            {
+                Console.WriteLine("Niet alle vissen zijn roofdieren");
+            }
+
+            //De namenlijst
+            List<string> fishNames = fish.Select(f => f.Name).ToList();
+
+            //Maak een list van enkel de namen. Ga na of er een vis is die Nemo heet 
+            bool nemoExists = fishNames.Contains("Nemo");
+            if (nemoExists)
+            {
+                Console.WriteLine("You found Nemo");
+            } else
+            {
+                Console.WriteLine("Could not find Nemo");
+            }
+
+            //Maak een list van enkel de namen. Vind de eerste naam die met C begint.
+            string firstCName = fishNames.First(f => f.StartsWith("C"));
+
+            //Ga na of er één Blobfish is (en niet meer).
+            Fish blobfish = fish.SingleOrDefault(f => f.Species == "Blobfish");
+            if (blobfish != null)
+            {
+                Console.WriteLine("Er is een blobfish");
+            } else
+            {
+                Console.WriteLine("Er is geen blobfish");
+            }
+
+            //Zoek de vis die het minst diep leeft
+            Fish shallowfish = fish.MinBy(f => f.DepthMeters);
+            Console.WriteLine($"Deze vis leeft het hoogst: {shallowfish.Name} - ({shallowfish.DepthMeters}m)");
+
+            //Zoek de vis die het diepst leeft
+            Fish deepfish = fish.MaxBy(f => f.DepthMeters);
+            Console.WriteLine($"Deze vis leeft het diepst: {deepfish.Name} - ({deepfish.DepthMeters}m)");
+
+            //Selecteer de eerste 5 vissen uit de lijst
+            var firstFive = fish.Take(5);
+
+
+            //Negeer de eerste 10 vissen en selecteer de rest 
+            var skipTen = fish.Skip(10);
         }
-
-
-
 
     }
 }
